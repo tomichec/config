964183355e7b2763c672a5175ffb5dd617824e07
Tomas Stary <tomas@stary.co.uk>
2023-08-09 12:57:56 +0200

    Init guix system.scm config

---
+++ guix/config.scm
@@ -0,0 +1,62 @@
+;; This is an operating system configuration generated
+;; by the graphical installer.
+;;
+;; Once installation is complete, you can learn and modify
+;; this file to tweak the system configuration, and pass it
+;; to the 'guix system reconfigure' command to effect your
+;; changes.
+
+
+;; Indicate which modules to import to access the variables
+;; used in this configuration.
+(use-modules (gnu))
+(use-service-modules cups desktop networking ssh xorg)
+
+(operating-system
+  (locale "en_GB.utf8")
+  (timezone "Europe/Berlin")
+  (keyboard-layout (keyboard-layout "us" "altgr-intl"))
+  (host-name "virt")
+
+  ;; The list of user accounts ('root' is implicit).
+  (users (cons* (user-account
+                  (name "bob")
+                  (comment "Bob")
+                  (group "users")
+                  (home-directory "/home/bob")
+                  (supplementary-groups '("wheel" "netdev" "audio" "video")))
+                %base-user-accounts))
+
+  ;; Packages installed system-wide.  Users can also install packages
+  ;; under their own account: use 'guix search KEYWORD' to search
+  ;; for packages and 'guix install PACKAGE' to install a package.
+  (packages (append (list (specification->package "nss-certs"))
+                    %base-packages))
+
+  ;; Below is the list of system services.  To search for available
+  ;; services, run 'guix system search KEYWORD' in a terminal.
+  (services
+   (append (list (service gnome-desktop-service-type)
+                 (set-xorg-configuration
+                  (xorg-configuration (keyboard-layout keyboard-layout))))
+
+           ;; This is the default list of services we
+           ;; are appending to.
+           %desktop-services))
+  (bootloader (bootloader-configuration
+                (bootloader grub-bootloader)
+                (targets (list "/dev/sda"))
+                (keyboard-layout keyboard-layout)))
+  (swap-devices (list (swap-space
+                        (target (uuid
+                                 "7da0d2fa-d203-4945-82f9-a168e3699b69")))))
+
+  ;; The list of file systems that get "mounted".  The unique
+  ;; file system identifiers there ("UUIDs") can be obtained
+  ;; by running 'blkid' in a terminal.
+  (file-systems (cons* (file-system
+                         (mount-point "/")
+                         (device (uuid
+                                  "e605d352-0bc2-4d4e-9217-a58c4f222848"
+                                  'ext4))
+                         (type "ext4")) %base-file-systems)))
