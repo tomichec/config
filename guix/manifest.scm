afba9e04d6784a7423d501c5698c3ff6b250b6be
Tomas Stary <tomas@stary.co.uk>
2023-08-09 14:08:42 +0200

    Init guix manifest.scm

---
+++ guix/manifest.scm
@@ -0,0 +1,31 @@
+;; What follows is a "manifest" equivalent to the command line you gave.
+;; You can store it in a file that you may then pass to any 'guix' command
+;; that accepts a '--manifest' (or '-m') option.
+
+(specifications->manifest
+  (list
+   ;; command line utils
+   "findutils"
+   "colordiff"
+   "ripgrep"
+   "glibc-locales"
+   "htop"
+
+   ;; audio-visual
+   "mpv"
+
+   ;; editors
+   "emacs"
+   "vim"
+
+   ;; development
+   "git"
+
+   ;; internet
+   "remmina"
+   "openssh"
+   "openvpn"
+   "nmap"
+   "hexchat"
+   "icedove"
+   "icecat"))
